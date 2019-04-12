fa420a15bb2af2e1f62e0eadb2b1970684ea5993
Davide Sottara <dsotty@gmail.com>
2019-04-11 22:49:35 -0500

    License headers

--- kmdp-artifact-repository-impl/src/test/java/edu/mayo/kmdp/repository/artifact/KnowledgeArtifactRepositoryTest.java
+++ kmdp-artifact-repository-impl/src/test/java/edu/mayo/kmdp/repository/artifact/KnowledgeArtifactRepositoryTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright © 2018 Mayo Clinic (RSTKNOWLEDGEMGMT@mayo.edu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package edu.mayo.kmdp.repository.artifact;
 
 import static edu.mayo.kmdp.id.helper.DatatypeHelper.uri;
@@ -11,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import edu.mayo.kmdp.repository.artifact.jcr.JcrDao;
 import edu.mayo.kmdp.repository.artifact.jcr.JcrKnowledgeArtifactRepository;
+import edu.mayo.kmdp.repository.artifact.server.Swagger2SpringBoot;
 import java.net.URI;
 import java.nio.file.Path;
 import java.util.Collections;
