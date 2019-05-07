4ddc5e676229a4767762121536de26e780b083d3
Kevin Peterson <peterson.kevin@mayo.edu>
2019-05-07 12:03:31 -0500

    Updated azure deploy.

--- azure.ps1
+++ azure.ps1
@@ -1,4 +1,4 @@
-$filePath = "kmdp-artifact-repository-impl/target/*.war"
+$filePath = "$(System.DefaultWorkingDirectory)/kmdp-artifact-repository-impl/target/*.war"
 $apiUrl = "https://kmdp-dev-kartrs.scm.azurewebsites.net/api/wardeploy"
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
 Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method POST -InFile $filePath -ContentType "multipart/form-data"
