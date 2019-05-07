fc468d595da48b10e37574a617793e26f4bd57c1
Kevin Peterson <peterson.kevin@mayo.edu>
2019-05-07 12:11:39 -0500

    Updated azure deploy.

--- azure.ps1
+++ azure.ps1
@@ -1,3 +1,3 @@
-$apiUrl = "https://kmdp-dev-kartrs.scm.azurewebsites.net/api/wardeploy"
+$apiUrl = "https://kmdp-dev-kartrs.mayo.edu/api/wardeploy"
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
 Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method POST -InFile $filePath -ContentType "multipart/form-data"
